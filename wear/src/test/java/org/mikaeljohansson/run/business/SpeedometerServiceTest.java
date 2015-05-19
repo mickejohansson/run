73fe97245fbb1e8c4c96101756f3913030ebfcb5
Mikael Johansson <mickejohansson@ustwo.com>
2015-05-19 22:07:31 +0200

    Added new test

--- wear/src/test/java/org/mikaeljohansson/run/business/SpeedometerServiceTest.java
+++ wear/src/test/java/org/mikaeljohansson/run/business/SpeedometerServiceTest.java
@@ -9,6 +9,9 @@ import org.mikaeljohansson.run.data.LocationRepository;
 import org.mikaeljohansson.run.data.LocationRepositoryWrapper;
 import org.mockito.Mock;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import rx.Observer;
 import rx.observers.TestObserver;
 import rx.subjects.PublishSubject;
@@ -49,5 +52,42 @@ public class SpeedometerServiceTest extends BaseTest {
         when(mockLocation.getSpeed()).thenReturn(speed);
         mLocationSubject.onNext(mockLocation);
         verify(delegate).onNext(speed);
+    }
+
+    @Test
+    public void onReceiveMultipleLocations_PassOnMultipleSpeeds() {
+        Observer<Float> delegate = mock(Observer.class);
+        TestObserver<Float> testObserver = new TestObserver(delegate);
+        mSpeedometerService.getCurrentSpeedObservable().subscribe(testObserver);
+
+        List<Float> speeds = new ArrayList<Float>();
+        speeds.add(2.0f);
+        speeds.add(2.1f);
+        speeds.add(2.2f);
+        speeds.add(2.3f);
+        speeds.add(2.4f);
+
+        Location mockLocation1 = mock(Location.class);
+        when(mockLocation1.getSpeed()).thenReturn(speeds.get(0));
+
+        Location mockLocation2 = mock(Location.class);
+        when(mockLocation2.getSpeed()).thenReturn(speeds.get(1));
+
+        Location mockLocation3 = mock(Location.class);
+        when(mockLocation3.getSpeed()).thenReturn(speeds.get(2));
+
+        Location mockLocation4 = mock(Location.class);
+        when(mockLocation4.getSpeed()).thenReturn(speeds.get(3));
+
+        Location mockLocation5 = mock(Location.class);
+        when(mockLocation5.getSpeed()).thenReturn(speeds.get(4));
+
+        mLocationSubject.onNext(mockLocation1);
+        mLocationSubject.onNext(mockLocation2);
+        mLocationSubject.onNext(mockLocation3);
+        mLocationSubject.onNext(mockLocation4);
+        mLocationSubject.onNext(mockLocation5);
+
+        testObserver.assertReceivedOnNext(speeds);
     }
 }
